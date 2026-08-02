267d70b4e1ebb4a590662edf2e330d649f64ba8a
Alfred <alfred.otienoh@gmail.com>
2026-08-02 06:13:29 +0300

    add http requests

---
+++ api.py
@@ -0,0 +1,7 @@
+import requests
+response = requests.get("https://jsonplaceholder.typicode.com/users/1")
+print(response.status_code)
+print(response.json())
+print(response.text)
+data = response.json()
+print(data["name"])
